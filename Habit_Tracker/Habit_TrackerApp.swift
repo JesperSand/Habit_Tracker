a091b2adc606e1e6b8109ab3f1590312158aa6ed
Jesper Sand <jespersand@jespers-air.home>
2024-05-03 13:17:11 +0200

    first commit

---
+++ Habit_Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit_Tracker
+//
+//  Created by Jesper Sand on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
